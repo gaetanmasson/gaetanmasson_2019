6dba1fb387d627cc104547b69e21a7c6042623e9
Gaetan Masson <hello@gaetanmasson.me>
2019-09-02 09:24:17 +0800

    fixed watch script

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-'use strict';
-
 // require gulp and gulp-sass
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 // compile sass
-gulp.task('sass', function () { 
-    gulp.src('sass/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(''));
+gulp.task('sass', () => { 
+    return gulp.src('sass/style.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('.'));
 });
 
 //compile and watch 
-gulp.task('watch', function() {
-    gulp.watch('sass/*/*.scss', gulp.series('sass'));
+gulp.task('watch', () => {
+    gulp.watch('sass/*/*.scss', (done) => {
+      gulp.series('sass')(done);
+    });
 });
