a6991e1bb46d1994e58e64d5d1fb73d53268b89c
Gaetan Masson <hello@gaetanmasson.me>
2019-09-23 10:11:46 +0800

    Added container to banner

--- header.php
+++ header.php
@@ -41,30 +41,32 @@
 				</div>
 			</nav><!-- #site-navigation -->
 		</div>
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				$front_page_title = get_field( "front_page_title" );
-				?>
-				<h1 class="site-title"><?php echo ($front_page_title ? $front_page_title : 'Headline missing'); ?></h1>
+		<div class="grid-container">
+			<div class="site-branding">
 				<?php
-			elseif ( is_page('about') ) :
-				$about_page_title = get_field( "about_page_title" );
-				?>
-				<h1 class="about-title"><?php echo ($about_page_title ? $about_page_title : 'Headline missing'); ?></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$gaetanmasson_2019_description = get_bloginfo( 'description', 'display' );
-			if ( $gaetanmasson_2019_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $gaetanmasson_2019_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					$front_page_title = get_field( "front_page_title" );
+					?>
+					<h1 class="site-title"><?php echo ($front_page_title ? $front_page_title : 'Headline missing'); ?></h1>
+					<?php
+				elseif ( is_page('about') ) :
+					$about_page_title = get_field( "about_page_title" );
+					?>
+					<h1 class="about-title"><?php echo ($about_page_title ? $about_page_title : 'Headline missing'); ?></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$gaetanmasson_2019_description = get_bloginfo( 'description', 'display' );
+				if ( $gaetanmasson_2019_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $gaetanmasson_2019_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
