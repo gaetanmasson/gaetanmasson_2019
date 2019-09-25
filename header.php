f6637c22a8b4b4dd7fc73eb8ebda4a467a90b492
Gaetan Masson <hello@gaetanmasson.me>
2019-09-25 18:08:46 +0800

    Created CSS Grid extention

--- header.php
+++ header.php
@@ -25,82 +25,78 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'gaetanmasson_2019' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="grid-container">
-			<nav id="site-navigation" class="main-navigation">
-				<div class="main-navigation__left">
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				</div>
-				<div class="main-navigation__right">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-						<figure class="icon__menu"><?php echo file_get_contents( get_template_directory() . "/assets/images/icon__menu.svg" ); ?></figure>
-						<figure class="icon__close"><?php echo file_get_contents( get_template_directory() . "/assets/images/icon__close.svg" ); ?></figure>
-					</button>
+		<nav id="site-navigation" class="main-navigation">
+			<div class="main-navigation__left">
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			</div>
+			<div class="main-navigation__right">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<figure class="icon__menu"><?php echo file_get_contents( get_template_directory() . "/assets/images/icon__menu.svg" ); ?></figure>
+					<figure class="icon__close"><?php echo file_get_contents( get_template_directory() . "/assets/images/icon__close.svg" ); ?></figure>
+				</button>
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
+			</div>
+		</nav><!-- #site-navigation -->
+		<div class="site-branding">
+			<?php
+			if ( is_front_page() && is_home() ) :
+				$frontpage__title =  get_field('frontpage__title', 'option');
+				$frontpage__tagline = get_field('frontpage__tagline', 'option');
+				$frontpage__tagline_link = get_field('frontpage__tagline-link', 'option');
+				
+				if( $frontpage__tagline_link ) {
+					$frontpage__tagline_link_url = $frontpage__tagline_link['url'];
+					$frontpage__tagline_link_title = $frontpage__tagline_link['title'];
+					$frontpage__tagline_link_target = $frontpage__tagline_link['target'] ? $frontpage__tagline_link['target'] : '_self';
+				}
+				?>
 					<?php
-					wp_nav_menu( array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					) );
+					if ( $frontpage__title ) :
 					?>
-				</div>
-			</nav><!-- #site-navigation -->
-		</div>
-		<div class="grid-container">
-			<div class="site-branding">
-				<?php
-				if ( is_front_page() && is_home() ) :
-					$frontpage__title =  get_field('frontpage__title', 'option');
-					$frontpage__tagline = get_field('frontpage__tagline', 'option');
-					$frontpage__tagline_link = get_field('frontpage__tagline-link', 'option');
-					
-					if( $frontpage__tagline_link ) {
-						$frontpage__tagline_link_url = $frontpage__tagline_link['url'];
-						$frontpage__tagline_link_title = $frontpage__tagline_link['title'];
-						$frontpage__tagline_link_target = $frontpage__tagline_link['target'] ? $frontpage__tagline_link['target'] : '_self';
-					}
+						<h1 class="frontpage__title"><?php echo $frontpage__title; ?></h1>
+					<?php
+					else :
 					?>
-						<?php
-						if ( $frontpage__title ) :
-						?>
-							<h1 class="frontpage__title"><?php echo $frontpage__title; ?></h1>
-						<?php
-						else :
-						?>
-							<h1 class="frontpage__title"><?php echo "Title is missing"; ?></h1>
-						<?php
-						endif;
+						<h1 class="frontpage__title"><?php echo "Title is missing"; ?></h1>
+					<?php
+					endif;
 
-						if ( $frontpage__tagline ) :
-						?>
-							<p class="frontpage__tagline"><?php echo $frontpage__tagline . " "; ?>
-								<?php
-								if ($frontpage__tagline_link) :
-								?>
-									<a class="button" href="<?php echo esc_url($frontpage__tagline_link_url); ?>" target="<?php echo esc_attr($frontpage__tagline_link_target); ?>"><?php echo esc_html($frontpage__tagline_link_title); ?></a>
-								<?php
-								endif;
-								?>
-							</p>
-						<?php
-						else :
-							?>
-							<p class="frontpage__tagline"><?php echo "Tagline is missing"; ?></p>
-						<?php
-						endif;
+					if ( $frontpage__tagline ) :
 					?>
-				<?php
-				elseif ( is_page('about') ) :
-					$about_page__title = get_field( "about_page__title" );
+						<p class="frontpage__tagline"><?php echo $frontpage__tagline . " "; ?>
+							<?php
+							if ($frontpage__tagline_link) :
+							?>
+								<a class="button" href="<?php echo esc_url($frontpage__tagline_link_url); ?>" target="<?php echo esc_attr($frontpage__tagline_link_target); ?>"><?php echo esc_html($frontpage__tagline_link_title); ?></a>
+							<?php
+							endif;
+							?>
+						</p>
+					<?php
+					else :
+						?>
+						<p class="frontpage__tagline"><?php echo "Tagline is missing"; ?></p>
+					<?php
+					endif;
 				?>
-					<h1 class="about-title"><?php echo ($about_page_title ? $about_page_title : 'Headline missing'); ?></h1>
-				<?php
-				else :
-				?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-				endif;
-				?>
-			</div><!-- .site-branding -->
-		</div>
+			<?php
+			elseif ( is_page('about') ) :
+				$about_page__title = get_field( "about_page__title" );
+			?>
+				<h1 class="about-title"><?php echo ($about_page_title ? $about_page_title : 'Headline missing'); ?></h1>
+			<?php
+			else :
+			?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+			?>
+		</div><!-- .site-branding -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
