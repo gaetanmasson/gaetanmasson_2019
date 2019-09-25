aa2ddd5111bf9ebf08b443676277175b998294ca
Gaetan Masson <hello@gaetanmasson.me>
2019-09-25 15:30:09 +0800

    Correct wrong gulp paths

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ const sass = require('gulp-sass');
 
 // compile sass
 gulp.task('sass', () => { 
-    return gulp.src('/assets/sass/style.scss')
+    return gulp.src('assets/sass/style.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('/assets/css'));
+      .pipe(gulp.dest('assets/css'));
 });
 
 //compile and watch 
 gulp.task('watch', () => {
-    gulp.watch('/assets/sass/*/*.scss', (done) => {
+    gulp.watch('assets/sass/*/*/*.scss', (done) => {
       gulp.series('sass')(done);
     });
 });
