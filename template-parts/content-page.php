7bdaaea4e5d1366bd78ba22c386c85bf0add6003
Gaetan Masson <hello@gaetanmasson.me>
2019-09-30 17:43:18 +0800

    Modified page template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,21 +10,22 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<header class="entry__header">
+		<figure class="entry__image"><?php gaetanmasson_2019_post_thumbnail(); ?></figure>
+		<?php the_title( '<h1 class="entry__title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php gaetanmasson_2019_post_thumbnail(); ?>
+	<div class="entry__content-container">
+		<div class="entry__content">
+			<?php
+			the_content();
 
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gaetanmasson_2019' ),
-			'after'  => '</div>',
-		) );
-		?>
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gaetanmasson_2019' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div><!-- .entry-content -->
 	</div><!-- .entry-content -->
 
 	<?php if ( get_edit_post_link() ) : ?>
