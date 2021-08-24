bc51122c29865da8afd7832e2e653e83118ea845
Gaetan Masson <hello@gaetanmasson.me>
2021-08-24 17:35:15 +0100

    Changed meta and footer container to be full width

--- template-parts/content.php
+++ template-parts/content.php
@@ -41,73 +41,77 @@
 
 	<?php endif; ?>
 
-	</header><!-- .entry-header -->
+	</header><!-- .entry__header -->
 	<div class="entry__meta-container">
-		<div class="entry__meta">
+		<div class="entry__meta-grid">
+			<div class="entry__meta">
 
-			<?php
-			$post__role = get_field('post__role');
-			$post__length = get_field('post__length');
-			$post__team_size = get_field('post__team-size');
+				<?php
+				$post__role = get_field('post__role');
+				$post__length = get_field('post__length');
+				$post__team_size = get_field('post__team-size');
 
-			if( $post__role ):
-			?>
+				if( $post__role ):
+				?>
 
-			<div class="entry__meta-item">
-				<span class="entry__meta-title">My role</span>
-				<span class="entry__meta-content"><?php echo $post__role; ?></span>
+				<div class="entry__meta-item">
+					<span class="entry__meta-title">My role</span>
+					<span class="entry__meta-content"><?php echo $post__role; ?></span>
+				</div>
+
+				<?php 
+				endif;
+
+				if( $post__length ):
+				?>
+
+				<div class="entry__meta-item">
+					<span class="entry__meta-title">Duration</span>
+					<span class="entry__meta-content"><?php echo $post__length; ?></span>
+				</div>
+
+				<?php 
+				endif;
+
+				if( $post__team_size ):
+				?>
+
+				<div class="entry__meta-item">
+					<span class="entry__meta-title">Team size</span>
+					<span class="entry__meta-content"><?php echo $post__team_size; ?></span>
+				</div>
+
+				<?php 
+				endif;
+				?>
+
 			</div>
-
-			<?php 
-			endif;
-
-			if( $post__length ):
-			?>
-
-			<div class="entry__meta-item">
-				<span class="entry__meta-title">Duration</span>
-				<span class="entry__meta-content"><?php echo $post__length; ?></span>
-			</div>
-
-			<?php 
-			endif;
-
-			if( $post__team_size ):
-			?>
-
-			<div class="entry__meta-item">
-				<span class="entry__meta-title">Team size</span>
-				<span class="entry__meta-content"><?php echo $post__team_size; ?></span>
-			</div>
-
-			<?php 
-			endif;
-			?>
-
 		</div>
 	</div>
 	<div class="entry__content-container">
-		<div class="entry__content">
+		<div class="entry__content-grid">
+			<div class="entry__content">
 
-			<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'gaetan_masson_design' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
+				<?php
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'gaetan_masson_design' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gaetan_masson_design' ),
-				'after'  => '</div>',
-			) );
-			?>
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gaetan_masson_design' ),
+					'after'  => '</div>',
+				) );
+				?>
+			</div>
 		</div>
-	</div><!-- .entry-content -->
+	</div><!-- .entry__content-container -->
 </article><!-- #post-<?php the_ID(); ?> -->
--- template-parts/post-navigation.php
+++ template-parts/post-navigation.php
@@ -1,27 +1,29 @@
 <div class="entry__navigation-container">
-    <div class="entry__navigation">
-        <p>Next case study</p>
-        <?php
-        $nextPost = get_next_post();
-                    
-        if( $nextPost ) :
-            $nextPostID = $nextPost->ID;
-        ?>
+    <div class="entry__navigation-grid">
+        <div class="entry__navigation">
+            <p>Next case study</p>
+            <?php
+            $nextPost = get_next_post();
+                        
+            if( $nextPost ) :
+                $nextPostID = $nextPost->ID;
+            ?>
 
-            <a class="prev-post" href="<?php echo get_permalink( $nextPostID ); ?>"><?php echo $nextPost->post_title; ?></a>
+                <a class="prev-post" href="<?php echo get_permalink( $nextPostID ); ?>"><?php echo $nextPost->post_title; ?></a>
 
-        <?php
-        else :
-            $first_post = get_posts( array(
-                'posts_per_page'   => 1,
-                'order' => 'ASC'
-            ) );
-        ?>
+            <?php
+            else :
+                $first_post = get_posts( array(
+                    'posts_per_page'   => 1,
+                    'order' => 'ASC'
+                ) );
+            ?>
 
-            <a class="prev-post" href="<?php echo get_permalink( $first_post[0]->ID ); ?>"><?php echo get_the_title( $first_post[0]->ID ); ?></a>
+                <a class="prev-post" href="<?php echo get_permalink( $first_post[0]->ID ); ?>"><?php echo get_the_title( $first_post[0]->ID ); ?></a>
 
-        <?php
-        endif;
-        ?>
+            <?php
+            endif;
+            ?>
+        </div>
     </div>
 </div>
